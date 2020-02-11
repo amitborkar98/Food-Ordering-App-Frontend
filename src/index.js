3d1116440d0aec7c975415aface073c112f59e20
amitborkar98 <amitborkar98@gmail.com>
2020-02-11 13:30:21 +0530

    Created Header component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Header from './common/header/Header';
 
 ReactDOM.render(
-    <div>
-        Food Ordering App
-    </div>,
-    document.getElementById('root')
+    <Header/>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
