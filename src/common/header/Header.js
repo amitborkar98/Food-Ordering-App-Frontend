1e7bb08e4c599d1820fae692e7f12129b7f7f2ec
amitborkar98 <amitborkar98@gmail.com>
2020-02-11 13:52:27 +0530

    Added the FastfoodIcon in the Header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,15 @@
 import React ,{Component} from 'react';
 import './Header.css';
+import FastfoodIcon from '@material-ui/icons/Fastfood';
 
 class Header extends Component{
     render(){
         return(
             <div>
                <header className="app-header">
-
+                   <div className="logo">
+                        <FastfoodIcon fontSize="large" htmlColor="white"/> 
+                    </div>
                </header>
             </div>
         );
