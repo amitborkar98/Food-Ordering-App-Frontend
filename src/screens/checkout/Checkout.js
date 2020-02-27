41eab1a24de452c9638cebe887c2aa2bdf99c18a
amitborkar98 <amitborkar98@gmail.com>
2020-02-27 12:54:06 +0530

    Added Tabs in the Delivery step

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -6,6 +6,8 @@ import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import StepContent from '@material-ui/core/StepContent';
 import Typography from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 class Checkout extends Component{
 
@@ -15,9 +17,13 @@ class Checkout extends Component{
             activeStep: 0,
             deliveryActive: true,
             deliveryCompleted: false,
-
+            value: 0,
 
         }
+    }
+    
+    tabChangeHandler = (event, value) =>{
+        this.setState({value});
     }
 
     render(){
@@ -25,28 +31,46 @@ class Checkout extends Component{
             <div>
                 <Header/>
                 <div className="checkout-main-conatiner">
-                    <Stepper activeStep={this.state.activeStep} orientation="vertical">
-                        <Step active={this.state.deliveryActive} completed={this.state.deliveryCompleted}>
-                            <StepLabel> 
-                                <Typography variant="body1" component="h1">
-                                    Delivery
-                                </Typography>
-                            </StepLabel>
-                            <StepContent>
-                           
-                            </StepContent>
-                        </Step>
-                        <Step>
-                            <StepLabel> 
-                                <Typography variant="body1" component="h1">
-                                    Payment
-                                </Typography>
-                            </StepLabel>
-                            <StepContent>
-                           
-                            </StepContent>
-                        </Step>
-                    </Stepper>
+                    <div className="steps">
+                        <Stepper activeStep={this.state.activeStep} orientation="vertical">
+                            <Step active={this.state.deliveryActive} completed={this.state.deliveryCompleted}>
+                                <StepLabel> 
+                                    <Typography variant="body1" component="h1">
+                                        Delivery
+                                    </Typography>
+                                </StepLabel>
+                                <StepContent>
+                                <Tabs className="tabs" value={this.state.value} onChange={this.tabChangeHandler}>
+                                    <Tab style={{color:"white"}} label="EXISTING ADDRESS" />
+                                    <Tab style={{color:"white"}} label="NEW ADDRESS" />
+                                </Tabs>
+                                {this.state.value === 0 &&
+                                <div>
+
+                                </div>
+                                }  
+                                {this.state.value === 1 &&
+                                <div>
+                                    
+                                </div>
+                                }       
+                                </StepContent>
+                            </Step>
+                            <Step>
+                                <StepLabel> 
+                                    <Typography variant="body1" component="h1">
+                                        Payment
+                                    </Typography>
+                                </StepLabel>
+                                <StepContent>
+                            
+                                </StepContent>
+                            </Step>
+                        </Stepper>
+                    </div>
+                    <div>
+
+                    </div>
                 </div>
             </div>
         );
